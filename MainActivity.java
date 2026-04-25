04dbeb28d1565e4cb376f9515a2f7bad6ccc34ac
monirol4664-glitch <monirol4664@gmail.com>
2026-04-25 21:03:01 +0600

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -4,16 +4,45 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
+import android.webkit.WebChromeClient;
+import android.view.KeyEvent;
 
 public class MainActivity extends Activity {
+    private WebView webView;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WebView webView = new WebView(this);
+        
+        webView = new WebView(this);
         WebSettings webSettings = webView.getSettings();
+        
+        // Optimize for Python IDE
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        
+        // Enable debugging
+        WebView.setWebContentsDebuggingEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        
+        // Load the Python IDE
         webView.loadUrl("file:///android_asset/index.html");
         setContentView(webView);
+    }
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
